64c997c7e9178860e2009244d4d9faf4b4cb8f17
Nick <nickorsk2017@gmail.com>
2022-01-11 13:13:45 +0500

    fix

--- doc/src/ui/HelpUs/HelpUs.tsx
+++ doc/src/ui/HelpUs/HelpUs.tsx
@@ -8,7 +8,7 @@ export const HelpUs = () => {
 
     return (
       <div className={styles.container}>Do you like it ? Please support us with a stars github 
-        <a className="github-button" href="https://github.com/nickorsk2017/formMOD" data-color-scheme="no-preference: light; light: light; dark: light_high_contrast;" data-icon="octicon-star" data-size="large" aria-label="Star ntkme/github-buttons on GitHub">Star</a>
+        <a className="github-button" href="https://github.com/nickorsk2017/formMOD" data-color-scheme="no-preference: light; light: light; dark: light_high_contrast;" data-icon="octicon-star" data-size="large" aria-label="Star /nickorsk2017/formMOD on GitHub">Star</a>
       </div>
     )
 }
